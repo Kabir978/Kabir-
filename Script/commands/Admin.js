3513a66da85763ab74ac46bb9a05656a9015c82a
Kabir978 <digantakabir008@gmail.com>
2025-11-19 19:36:20 +0600

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -7,7 +7,7 @@ module.exports.config = {
  name: "admin",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦",
+ credits: "Eftekhar kabir",
  description: "Show Owner Info",
  commandCategory: "info",
  usages: "admin",
